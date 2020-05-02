d4ac079ba7651fc219613b5382c42e14d5693fef
Emmanuelle Jaldorau <e.jaldorau@gmail.com>
2020-05-02 14:50:47 +0200

    style(navigation): created layout for desktop

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -6,8 +6,18 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 function Navigation(){
     return(
         <nav className="navigation">
-            <FontAwesomeIcon icon="bars" />
+            <FontAwesomeIcon icon="bars" className="navigation-mobile" />
             <h3>Cathy Boutiq</h3>
+            <div className="navigation-desktop">
+                <div>
+                    <p>Products</p>
+                    <p>Contact</p>
+                </div>
+                <div className="navigation-desktop-search">
+                    <FontAwesomeIcon icon="search" />
+                    <input type="text" />
+                </div>
+            </div>
             <FontAwesomeIcon icon="shopping-bag" />
         </nav>
     )
