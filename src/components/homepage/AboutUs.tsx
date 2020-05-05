d63ad2b827143a790c7d86d0074ef7030c28cd1d
Emmanuelle Jaldorau <e.jaldorau@gmail.com>
2020-05-05 21:19:03 +0200

    style(aboutUs):adjusted layout

--- src/components/homepage/AboutUs.tsx
+++ src/components/homepage/AboutUs.tsx
@@ -4,17 +4,21 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 function AboutUs(){
     return(
         <div className="about-us">
-            <div>
-                <FontAwesomeIcon icon="hands" className="about-us-icon" />
-                <p className="about-us-text">Culture sans pesticides, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-            </div>
-            <div>
-                <FontAwesomeIcon icon="map-marker-alt" className="about-us-icon" />
-                <p className="about-us-text">Production locale, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-            </div>
-            <div>
-                <FontAwesomeIcon icon="truck" className="about-us-icon" />
-                <p className="about-us-text">Lieux de pick up, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+            <h2>Information Utiles</h2>
+            <div className="title-decoration"></div>
+            <div className="about-us-tiles">
+                <div>
+                    <FontAwesomeIcon icon="hands" className="about-us-icon" />
+                    <p className="about-us-text">Culture sans pesticides, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                </div>
+                <div>
+                    <FontAwesomeIcon icon="map-marker-alt" className="about-us-icon" />
+                    <p className="about-us-text">Production locale, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                </div>
+                <div>
+                    <FontAwesomeIcon icon="truck" className="about-us-icon" />
+                    <p className="about-us-text">Lieux de pick up, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                </div>
             </div>
         </div>
     )
