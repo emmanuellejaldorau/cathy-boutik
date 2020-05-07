b524f6f2c0a3ec46afb4df40f2aa8926e9c33015
Emmanuelle Jaldorau <e.jaldorau@gmail.com>
2020-05-07 12:36:18 +0200

    feat(footer): created for desktop and mobile

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Navigation from './components/Navigation';
 import Homepage from './components/homepage/Homepage';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
       <Navigation />
       <Homepage />
+      <Footer/>
     </>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer(){
+    return(
+        <footer>
+            <p>Designé et developpé par Emmanuelle</p>
+        </footer>
+    )
+}
+
+export default Footer;
