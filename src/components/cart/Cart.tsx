782e73a79cf701c66a367f5ff56910d400568edc
Emmanuelle Jaldorau <e.jaldorau@gmail.com>
2020-05-13 15:17:30 +0200

    style(cart): created desktop and mobile versions

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import './Cart.css';
 
 function Cart(){
     return(
-        <main>
+        <main className="cart">
+            <div className="cart-section">
+                <p>Votre panier est vide</p>
+                <button className="cart-cta"> 
+                    <Link to="/produits">Découvrez nos produits</Link>
+                </button>
+            </div>
+            <div className="cart-section">
+                <p>Besoin d'aide?</p>
+                <button className="cart-cta">
+                    <Link to="/contact">Contactez-nous</Link>
+                </button>
 
+            </div>
         </main>
     )
 }
